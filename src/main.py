d964fe521a0e57201eb62806efe6e0f71d8d54e9
Ziqul <m.s.v.00a@gmail.com>
2018-09-27 10:23:42 +0300

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,26 @@
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import numpy as np
+
+from .terrain import Terrain
+
+
+def main():
+    terr = Terrain(8, 0.5)
+
+    terrshape = terr.map.shape
+
+    X = np.arange(terrshape[0])
+    Y = np.arange(terrshape[1])
+    X, Y = np.meshgrid(X, Y)
+    Z = terr.map
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(X, Y, Z, color='b')
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/terrain.py
@@ -0,0 +1,75 @@
+import numpy as np
+
+
+class Terrain(object):
+    def __init__(self, power, roughness):
+        self.edgesize = 2 ** power + 1
+        self.maxheight = self.edgesize - 1
+        self.map = np.full((self.edgesize, self.edgesize), self.maxheight / 2)
+        self.roughness = roughness
+
+        self.setcorners()
+        self.divide(self.edgesize - 1)
+
+    def setcorners(self):
+        """
+        setcorners()
+
+        Set initial values for points in the corners of the map.
+        Initial value is equal to half of the max value.
+        """
+        self.map[0, 0] = self.maxheight / 2
+        self.map[0, self.edgesize - 1] = self.maxheight / 2
+        self.map[self.edgesize - 1, 0] = self.maxheight / 2
+        self.map[self.edgesize - 1, self.edgesize - 1] = self.maxheight / 2
+
+    def divide(self, size):
+        half = size / 2
+        scale = self.roughness * size
+
+        if half < 1:
+            return
+
+        for y in np.arange(half, self.edgesize - 1, size):
+            for x in np.arange(half, self.edgesize - 1, size):
+                self.square(x, y, half, np.random.rand() * scale * 2 - scale)
+
+        for y in np.arange(0, self.edgesize, half):
+            for x in np.arange(np.mod(y + half, size), self.edgesize, size):
+                self.diamond(x, y, half, np.random.rand() * scale * 2 - scale)
+
+        self.divide(size / 2)
+
+    def square(self, x, y, size, offset):
+        average = np.mean([
+            self.map[int(x - size), int(y - size)],
+            self.map[int(x + size), int(y - size)],
+            self.map[int(x + size), int(y + size)],
+            self.map[int(x - size), int(y + size)]
+        ])
+
+        self.map[int(x), int(y)] = average + offset
+
+    def diamond(self, x, y, size, offset):
+        points = []
+
+        try:
+            points.append(self.map[int(x), int(y - size)])
+        except IndexError as ie:
+            pass
+        try:
+            points.append(self.map[int(x + size), int(y)])
+        except IndexError as ie:
+            pass
+        try:
+            points.append(self.map[int(x), int(y + size)])
+        except IndexError as ie:
+            pass
+        try:
+            points.append(self.map[int(x - size), int(y)])
+        except IndexError as ie:
+            pass
+
+        average = np.mean(points)
+
+        self.map[int(x), int(y)] = average + offset
